d2430c976240731c7bbe45da174e768f4edc69d3
JiePrass <renjieprass@gmail.com>
2024-08-12 09:49:04 +0700

    pertamax

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+export default function Footer({ items }) {
+    if (items.length === 0) {
+        return (
+            <footer className="stats">
+                Daftar Belanja Kosong brok
+            </footer>
+        )
+    }
+
+    const totalItems = items.length
+    const checkedItems = items.filter((item) => item.checked).length
+    const percentage = Math.round((checkedItems / totalItems) * 100)
+
+    return (
+        <footer className="stats">
+            Ada {totalItems} barang di daftar belanjaan, {checkedItems} barang sudah dibeli ({percentage}%)
+        </footer>
+    )
+}
